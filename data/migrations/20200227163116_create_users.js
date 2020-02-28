e473b38a1af9b5b89d3cb9de792b7aee6da0ea2c
[Arin Ramer] <[arinramer@gmail.com]>
2020-02-27 17:56:40 -0800

    Done

---
+++ data/migrations/20200227163116_create_users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', tbl => {
+      tbl.increments();
+      tbl.string('username');
+      tbl.string('password');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.createTable.dropTableIfExists();
+};
